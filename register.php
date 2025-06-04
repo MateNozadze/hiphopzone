2b36c7b416037a273b5643d49c8e9fd5a124580b
mate nozadze <nozadzemate17@gmail.com>
2025-06-04 22:54:52 +0400

    log/reg

---
+++ register.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1 class="title"><a href="index.php">Hip-Hop Zone</a></h1>
+    </header>
+    <main class="login-main">
+        <form action="#">
+            <input type="text" name="name" id="name" placeholder="enter name">
+            <input type="password" name="password" id="password" placeholder="enter password">
+            <input type="password" name="Re-enter-password" id="Re-enter-password" placeholder="Re-enter password">
+
+            <input class="register" type="submit" value="register">
+            <p class="have-no-acc">Already have an account? <a href="login.php"> log in</a></p>
+        </form>
+    </main>
+
+
+<?php include 'fragments/footer.php'; ?>
